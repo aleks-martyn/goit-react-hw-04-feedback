dbbcb6368e13f7db2a7b987bf335006f5fe91591
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-05-15 21:14:44 +0300

    Update FeedbackOptions.jsx

---
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -0,0 +1,21 @@
+import PropTypes from 'prop-types';
+import { Wrap, Button } from './FeedbackOptions.styled';
+
+export const FeedbackOptions = ({ options, onLeaveFeedback }) => (
+  <Wrap>
+    {options.map(option => (
+      <Button
+        key={option}
+        type="button"
+        onClick={() => onLeaveFeedback(option)}
+      >
+        <span>{option}</span>
+      </Button>
+    ))}
+  </Wrap>
+);
+
+FeedbackOptions.propTypes = {
+  options: PropTypes.arrayOf(PropTypes.string).isRequired,
+  onLeaveFeedback: PropTypes.func.isRequired,
+};
