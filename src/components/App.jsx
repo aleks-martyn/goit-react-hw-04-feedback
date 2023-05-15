3ecf097763b2465157b3ec7c79be2a721a6d1d8c
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-05-15 23:25:34 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,42 @@
+import { useState } from 'react';
+import { Section } from './Section';
 import { Container } from './App.styled';
 
 export const App = () => {
-  return <Container></Container>;
+  const [goodComment, setGoodComment] = useState(0);
+  const [neutralComment, setNeutralComment] = useState(0);
+  const [badComment, setBadComment] = useState(0);
+
+  const handleGoodCommentChange = event => {
+    setGoodComment(prevState => goodComment + 1);
+  };
+  const handleNeutralCommentChange = event => {
+    setNeutralComment(prevState => neutralComment + 1);
+  };
+  const handleBadCommentChange = event => {
+    setBadComment(prevState => badComment + 1);
+  };
+
+  return (
+    <Container>
+      <Section title="Please leave feedback">
+        <button type="button" onClick={handleGoodCommentChange}>
+          Good
+        </button>
+        <button type="button" onClick={handleNeutralCommentChange}>
+          Neutral
+        </button>
+        <button type="button" onClick={handleBadCommentChange}>
+          Bad
+        </button>
+      </Section>
+      <Section title="Statistics">
+        <div>
+          <p>Good: {goodComment}</p>
+          <p>Neutral: {neutralComment}</p>
+          <p>Bad: {badComment}</p>
+        </div>
+      </Section>
+    </Container>
+  );
 };
