0d5d772cc0b5c588122589bc18de9b8e3f11fcd3
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-05-16 00:34:06 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -9,33 +9,36 @@ export const App = () => {
   const [bad, setBad] = useState(0);
 
   const handleGoodClick = event => {
-    setGood(prevState => good + 1);
+    setGood(prevState => prevState + 1);
   };
   const handleNeutralClick = event => {
-    setNeutral(prevState => neutral + 1);
+    setNeutral(prevState => prevState + 1);
   };
   const handleBadClick = event => {
-    setBad(prevState => bad + 1);
+    setBad(prevState => prevState + 1);
   };
 
   const handleLeaveFeedback = label => {
     switch (label) {
       case 'good':
-        setGood(prevState => label + 1);
+        setGood(prevState => prevState + 1);
         break;
 
       case 'neutral':
-        setNeutral(prevState => label + 1);
+        setNeutral(prevState => prevState + 1);
         break;
 
       case 'bad':
-        setBad(prevState => label + 1);
+        setBad(prevState => prevState + 1);
         break;
 
       default:
         return;
     }
   };
+
+  const options = Object.keys({ good, neutral, bad })
+  console.log(options);
 
   return (
     <Container>
